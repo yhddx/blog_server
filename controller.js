8140869d1e4038f393868f5a147adac81b15109d
yhddx <286867796@qq.com>
2019-06-10 22:21:44 +0800

    add /api/ prefix

--- controller.js
+++ controller.js
@@ -5,19 +5,19 @@ const fs = require('fs');
 function addMapping(router, mapping) {
     for (var url in mapping) {
         if (url.startsWith('GET ')) {
-            var path = url.substring(4);
+            var path = "/api" + url.substring(4);
             router.get(path, mapping[url]);
             console.log(`register URL mapping: GET ${path}`);
         } else if (url.startsWith('POST ')) {
-            var path = url.substring(5);
+            var path = "/api" + url.substring(5);
             router.post(path, mapping[url]);
             console.log(`register URL mapping: POST ${path}`);
         } else if (url.startsWith('PUT ')) {
-            var path = url.substring(4);
+            var path = "/api" + url.substring(4);
             router.put(path, mapping[url]);
             console.log(`register URL mapping: PUT ${path}`);
         } else if (url.startsWith('DELETE ')) {
-            var path = url.substring(7);
+            var path = "/api" + url.substring(7);
             router.del(path, mapping[url]);
             console.log(`register URL mapping: DELETE ${path}`);
         } else {
